179b7aa3d0c9e46819b188ecc193bafe33a6829b
Marcin Lewandowski <marcin@saepia.net>
2010-09-06 19:59:52 +0200

    Added script to build ubuntu/debian packages

---
+++ build-debian-package.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+rm -rf libjack-ffi-ruby/
+
+mkdir libjack-ffi-ruby/
+mkdir libjack-ffi-ruby/DEBIAN
+echo "Package: libjack-ffi-ruby
+Version: `cat VERSION`
+Section: base
+Priority: optional
+Architecture: all
+Depends: libffi-ruby, libjack0
+Maintainer: marcin@saepia.net
+Description: Jack Audio Connection Kit bindings for ruby via FFI" > libjack-ffi-ruby/DEBIAN/control
+
+cp -r lib/ VERSION GPL3-LICENSE libjack-ffi-ruby/
+find libjack-ffi-ruby/ | grep '/\.' | xargs rm -rf
+
+dpkg --build libjack-ffi-ruby ./
