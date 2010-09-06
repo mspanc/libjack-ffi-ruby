0315da917bbf9861ebc2c3e94dc3147a98a36d70
Marcin Lewandowski <marcin@saepia.net>
2010-09-06 20:05:32 +0200

    Added ruby dependency to debian build script

--- build-debian-package.sh
+++ build-debian-package.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+rm -rf libjack-ffi-ruby1.8/
+
+mkdir libjack-ffi-ruby1.8/
+mkdir libjack-ffi-ruby1.8/DEBIAN
+echo "Package: libjack-ffi-ruby1.8
+Version: `cat VERSION`
+Section: base
+Priority: optional
+Architecture: all
+Depends: ruby, libffi-ruby, libjack0
+Maintainer: marcin@saepia.net
+Description: Jack Audio Connection Kit bindings for ruby via FFI" > libjack-ffi-ruby1.8/DEBIAN/control
+
+mkdir -p libjack-ffi-ruby1.8/usr/lib/ruby/1.8/
+cp -r lib/* libjack-ffi-ruby1.8/usr/lib/ruby/1.8/
+find libjack-ffi-ruby1.8/ | grep '/\.' | xargs rm -rf
+
+dpkg --build libjack-ffi-ruby1.8 ./
+rm -rf libjack-ffi-ruby1.8/
+
 rm -rf libjack-ffi-ruby/
 
 mkdir libjack-ffi-ruby/
@@ -8,11 +28,13 @@ Version: `cat VERSION`
 Section: base
 Priority: optional
 Architecture: all
-Depends: ruby, libffi-ruby, libjack0
+Depends: libjack-ffi-ruby1.8
 Maintainer: marcin@saepia.net
 Description: Jack Audio Connection Kit bindings for ruby via FFI" > libjack-ffi-ruby/DEBIAN/control
 
-cp -r lib/ VERSION GPL3-LICENSE libjack-ffi-ruby/
 find libjack-ffi-ruby/ | grep '/\.' | xargs rm -rf
 
 dpkg --build libjack-ffi-ruby ./
+rm -rf libjack-ffi-ruby/
+
+
