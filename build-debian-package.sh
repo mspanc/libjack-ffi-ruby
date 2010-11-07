3f55c4a4e46c280179ed9f116c80db7bfba95147
Marcin Lewandowski <marcin@marcin-laptop.(none)>
2010-11-07 19:24:17 +0100

    Fixed build debian package script for ubuntu 10.10

--- build-debian-package.sh
+++ build-debian-package.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+source /etc/lsb-release
+
+
+CURRENT_UBUNTU_VERSION=$(( ${DISTRIB_RELEASE%.*} * 100 + ${DISTRIB_RELEASE#*.} )) 
+JACKD2_MINIMAL_VERSION=$((1010)) # 10.10 converted to int
+
 rm -rf libjack-ffi-ruby1.8/
 
 mkdir libjack-ffi-ruby1.8/
@@ -7,10 +13,19 @@ echo "Package: libjack-ffi-ruby1.8
 Version: `cat VERSION`
 Section: base
 Priority: optional
-Architecture: all
-Depends: ruby, libffi-ruby, libjack0
-Maintainer: marcin@saepia.net
-Description: Jack Audio Connection Kit bindings for ruby via FFI" > libjack-ffi-ruby1.8/DEBIAN/control
+Architecture: all"  > libjack-ffi-ruby1.8/DEBIAN/control
+
+if [ $CURRENT_UBUNTU_VERSION -ge $JACKD2_MINIMAL_VERSION ] 
+then
+  echo "Building for Ubuntu >= 10.10, against JACK version 2"
+  echo "Depends: ruby, libffi-ruby, libjack-jackd2-0" >> libjack-ffi-ruby1.8/DEBIAN/control 
+else
+  echo "Building for Ubuntu < 10.10, against JACK version 1"
+  echo "Depends: ruby, libffi-ruby, libjack0" >> libjack-ffi-ruby1.8/DEBIAN/control 
+fi
+
+echo "Maintainer: marcin@saepia.net
+Description: Jack Audio Connection Kit bindings for ruby via FFI" >> libjack-ffi-ruby1.8/DEBIAN/control
 
 mkdir -p libjack-ffi-ruby1.8/usr/lib/ruby/1.8/
 cp -r lib/* libjack-ffi-ruby1.8/usr/lib/ruby/1.8/
